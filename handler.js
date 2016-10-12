51f385de304165d18b4e745dcb813f4520bae62d
Ahmet Sevki <ahmet@nalch.com>
2016-10-12 17:09:41 -0400

    using get, post and get with query params

--- handler.js
+++ handler.js
@@ -1,9 +1,21 @@
 'use strict';
 
 // Your first function handler
-module.exports.hello = (event, context, cb) => {
+module.exports.hellos = (event, context, cb) => {
+    console.log("hellos", JSON.stringify(event))
   /** cb is function (err,data)  */
-    cb(null, { message: 'Hello World', event, context });
+    cb(null, { message: 'Hellos To Everyone', event, context });
+};
+
+module.exports.postHello = (event, context, cb) => {
+    console.log("postHello", JSON.stringify(event))
+    cb(null, { message: 'Hello '.concat(event.body.username), event, context });
+};
+
+module.exports.getHello = (event, context, cb) => {
+    console.log("getHello", JSON.stringify(event))
+    cb(null, { message: 'Hello '.concat(event.path.username), event, context });
 };
 
 // You can add more handlers here, and reference them in serverless.yml
+
